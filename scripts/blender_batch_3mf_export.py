37f5c4182f12f2b37dca6da885901fca39ce62a7
Anson Liu <ansonl@users.noreply.github.com>
2023-07-16 21:38:24 -0400

    update script

--- scripts/blender_batch_3mf_export.py
+++ scripts/blender_batch_3mf_export.py
@@ -5,19 +5,19 @@
 
 import re, bpy
 
+regionAbbr = 'XX'
+
 def export3MF(abbr, printType, style, partNum):
   bpy.ops.object.select_all(action='DESELECT')
 
   for o in bpy.data.objects:
     # Check for given object names
-    if re.search(f'{abbr}-{printType}{"-(?:land-elevation|hydrography)" if printType == "dual" else ""}{"-" if len(style) > 0 else ""}{style}{"-p" if partNum > 0 else ""}{partNum}', o.name) is not None:
+    if re.search(f'{abbr}-{printType}{"-(?:land-elevation|hydrography)" if printType == "dual" else ""}{"-" if len(style) > 0 else ""}{style}{f"-p{partNum}" if partNum > 0 else ""}', o.name) is not None:
       o.select_set(True)
 
   # export objects
   if len(bpy.context.selected_objects) > 0:
     bpy.ops.export_mesh.threemf(filepath=f'K:/USAofPlasticv1/release_250m_v1/{abbr}/{abbr}-{printType}{"-" if len(style) > 0 else ""}{style}{"-p" if partNum > 0 else ""}{partNum}.3mf', use_selection=True)
-  
-regionAbbr = 'RI'
 
 # Export generated models
 export3MF(regionAbbr, 'single', '', 0)
