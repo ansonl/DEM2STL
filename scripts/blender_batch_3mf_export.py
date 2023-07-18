92cda95c5aefe6947f0d6d6d40f06ac5215debf3
Anson Liu <ansonl@users.noreply.github.com>
2023-07-18 00:10:43 -0400

    add import script and modal version

--- scripts/blender_batch_3mf_export.py
+++ scripts/blender_batch_3mf_export.py
@@ -6,14 +6,41 @@
 # This script automates the 3MF export of objects named in the below format. 
 # Format: Abbreviation-PrintType-(land-elevation|hydrography|)-Style-pPartNumber
 #   Abbreviation : Region abbreviation
-#   PrintType : single or dual [extrusion]
+#   PrintType (export) : single or dual [extrusion]
 #   land-elevation and hydrography are checked if dual PrintType
 #   Style : Special style e.g. transparent
 #   pPartNumber: p followed by the partNumber if objects are part of a multi-piece multi-print model. Use 0 if not in a multi-piece model.
 
-import re, bpy
+import os, re, bpy
 
-regionAbbr = 'XX'
+regionsTopDir = 'K:/USAofPlasticv1/release_250m_v1/'
+
+excludeList = ['AL','CO','ID','OR','MA','CA','FL','CT','WA']
+
+allFiles = os.listdir(regionsTopDir)
+
+def get_dir_size(path='.'):
+  total = 0
+  with os.scandir(path) as it:
+    for entry in it:
+      if entry.is_file():
+        total += entry.stat().st_size
+      elif entry.is_dir():
+        total += get_dir_size(entry.path)
+  return total
+
+allFiles.sort(key=lambda f: get_dir_size(regionsTopDir+f), reverse=False)
+
+regionList = allFiles
+
+#regionList = ['XX']
+
+def importSTL(abbr, printType, style):
+  bpy.ops.import_mesh.stl(filepath=f'K:/USAofPlasticv1/release_250m_v1/{abbr}/{abbr}-{printType}{"-land-elevation" if printType == "dual" else ""}{"-" if len(style) > 0 else ""}{style}.STL')
+
+  # import second model if dual PrintType
+  if printType == "dual":
+    bpy.ops.import_mesh.stl(filepath=f'K:/USAofPlasticv1/release_250m_v1/{abbr}/{abbr}-{printType}{"-hydrography" if printType == "dual" else ""}{"-" if len(style) > 0 else ""}{style}.STL')
 
 def export3MF(abbr, printType, style, partNum):
   bpy.ops.object.select_all(action='DESELECT')
@@ -27,13 +54,26 @@ def export3MF(abbr, printType, style, partNum):
   if len(bpy.context.selected_objects) > 0:
     bpy.ops.export_mesh.threemf(filepath=f'K:/USAofPlasticv1/release_250m_v1/{abbr}/{abbr}-{printType}{"-" if len(style) > 0 else ""}{style}{f"-p{partNum}" if partNum > 0 else ""}.3mf', use_selection=True)
 
-# Export generated models
-export3MF(regionAbbr, 'single', '', 0)
-export3MF(regionAbbr, 'dual', '', 0)
-export3MF(regionAbbr, 'dual', 'transparent', 0)
+for rAbbr in regionList:
+  # Skip if region is in exclude list
+  if rAbbr in excludeList:
+    continue
 
-# Export cut models
-export3MF(regionAbbr, 'dual', 'transparent', 1)
-export3MF(regionAbbr, 'dual', 'transparent', 2)
-export3MF(regionAbbr, 'dual', 'transparent', 3)
-export3MF(regionAbbr, 'dual', 'transparent', 4)
+  # Import and export the different generated models
+  importSTL(rAbbr, 'single', '')
+  export3MF(rAbbr, 'single', '', 0)
+  bpy.ops.object.delete() # delete the object afterwards to reduce unused memory usage
+
+  importSTL(rAbbr, 'dual', '')
+  export3MF(rAbbr, 'dual', '', 0)
+  bpy.ops.object.delete()
+
+  importSTL(rAbbr, 'dual', 'transparent')
+  export3MF(rAbbr, 'dual', 'transparent', 0)
+  bpy.ops.object.delete()
+
+  # Export cut models (optional)
+  export3MF(rAbbr, 'dual', 'transparent', 1)
+  export3MF(rAbbr, 'dual', 'transparent', 2)
+  export3MF(rAbbr, 'dual', 'transparent', 3)
+  export3MF(rAbbr, 'dual', 'transparent', 4)
---
+++ scripts/blender_batch_3mf_export_modal.py
@@ -0,0 +1,127 @@
+import bpy
+import os
+import re
+import time
+
+regionsTopDir = 'K:/USAofPlasticv1/release_250m_v1/'
+
+excludeList = ['AL','CO','ID','OR','MA','CA','FL','CT','WA']
+
+def importSTL(abbr, printType, style):
+  bpy.ops.import_mesh.stl(filepath=f'K:/USAofPlasticv1/release_250m_v1/{abbr}/{abbr}-{printType}{"-land-elevation" if printType == "dual" else ""}{"-" if len(style) > 0 else ""}{style}.STL')
+
+  # import second model if dual PrintType
+  if printType == "dual":
+    bpy.ops.import_mesh.stl(filepath=f'K:/USAofPlasticv1/release_250m_v1/{abbr}/{abbr}-{printType}{"-hydrography" if printType == "dual" else ""}{"-" if len(style) > 0 else ""}{style}.STL')
+
+def export3MF(abbr, printType, style, partNum):
+  bpy.ops.object.select_all(action='DESELECT')
+
+  for o in bpy.data.objects:
+    # Check for given object names
+    if re.search(f'{abbr}-{printType}{"-(?:land-elevation|hydrography)" if printType == "dual" else ""}{"-" if len(style) > 0 else ""}{style}{f"-p{partNum}" if partNum > 0 else ""}', o.name) is not None:
+      o.select_set(True)
+
+  # export objects
+  if len(bpy.context.selected_objects) > 0:
+    bpy.ops.export_mesh.threemf(filepath=f'K:/USAofPlasticv1/release_250m_v1/{abbr}/{abbr}-{printType}{"-" if len(style) > 0 else ""}{style}{f"-p{partNum}" if partNum > 0 else ""}.3mf', use_selection=True)
+
+class ModalTimerOperator(bpy.types.Operator):
+  """Operator which runs its self from a timer"""
+  bl_idname = "wm.modal_timer_operator"
+  bl_label = "Modal Timer Operator"
+
+  _regionProcessStartTime = 0
+  _regionList = []
+  _regionPos = 0
+  _variant = 0
+  _timer = None
+
+  def modal(self, context, event):
+    #self.report({'INFO'}, f'{self._regionPos} {self._variant} {len(self._regionList)}')
+    if event.type in {'ESC'} or self._regionPos == len(self._regionList):
+        self.report({'INFO'}, f'finished')
+        self._regionPos = 0
+        self.cancel(context)
+        return {'FINISHED'}
+
+    rAbbr = self._regionList[self._regionPos]
+
+    # Skip if region is in exclude list
+    if rAbbr in excludeList:
+        self._regionPos += 1
+        return {'PASS_THROUGH'}
+
+    variantProcessStartTime = time.monotonic()
+
+    if self._variant == 0:
+        self._regionProcessStartTime = time.monotonic()
+        # Import and export the different generated models
+        importSTL(rAbbr, 'single', '')
+        export3MF(rAbbr, 'single', '', 0)
+        bpy.ops.object.delete() # delete the object afterwards to reduce unused memory usage
+    elif self._variant == 1:
+        importSTL(rAbbr, 'dual', '')
+        export3MF(rAbbr, 'dual', '', 0)
+        bpy.ops.object.delete()
+    elif self._variant == 2:
+        importSTL(rAbbr, 'dual', 'transparent')
+        export3MF(rAbbr, 'dual', 'transparent', 0)
+        bpy.ops.object.delete()
+    elif self._variant == 3:
+        # Export cut models (optional)
+        export3MF(rAbbr, 'dual', 'transparent', 1)
+        export3MF(rAbbr, 'dual', 'transparent', 2)
+        export3MF(rAbbr, 'dual', 'transparent', 3)
+        export3MF(rAbbr, 'dual', 'transparent', 4)
+        self._regionPos += 1
+    
+    self.report({'INFO'}, f'{rAbbr} variant {self._variant} took {time.monotonic()-variantProcessStartTime}s')
+
+    self._variant += 1
+    if self._variant == 4:
+       self.report({'INFO'}, f'{rAbbr} total time {time.monotonic()-self._regionProcessStartTime}s')
+       self._variant = 0
+
+    return {'PASS_THROUGH'}
+
+  def execute(self, context):
+    self._regionList = os.listdir(regionsTopDir)
+
+    def get_dir_size(path='.'):
+        total = 0
+        with os.scandir(path) as it:
+            for entry in it:
+                if entry.is_file():
+                    total += entry.stat().st_size
+                elif entry.is_dir():
+                    total += get_dir_size(entry.path)
+        return total
+
+    self._regionList.sort(key=lambda f: get_dir_size(regionsTopDir+f), reverse=False)
+
+    #self._regionList = ['RI', 'DC']
+
+    wm = context.window_manager
+    self._timer = wm.event_timer_add(time_step=0.1, window=context.window)
+    wm.modal_handler_add(self)
+    return {'RUNNING_MODAL'}
+
+  def cancel(self, context):
+    wm = context.window_manager
+    wm.event_timer_remove(self._timer)
+
+
+def register():
+    bpy.utils.register_class(ModalTimerOperator)
+
+
+def unregister():
+    bpy.utils.unregister_class(ModalTimerOperator)
+
+
+if __name__ == "__main__":
+    register()
+
+    # test call
+    bpy.ops.wm.modal_timer_operator()
